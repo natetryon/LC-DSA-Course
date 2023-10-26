00b9a2eeadf3903dc86add17a1db93716b62fa36
Nathan Tryon <nstryon@alumni.ncsu.edu>
2023-10-26 06:00:23 -0400

    First commit, HelloWorld project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello GitHub, Copilot, Visual Studio, .NET, and Microsoft in general...");
+    }
+}
